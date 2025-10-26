ee5fe7715e89aea65cf14bb98bc6162440748538
luozhenyu0911 <1018954087@qq.com>
2025-10-26 21:13:15 +0800

    20251026

--- plot/20251023VeenPlot/veenplot.R
+++ plot/20251023VeenPlot/veenplot.R
@@ -1,19 +1,17 @@
+
+# install.packages("ggVennDiagram")
 library(ggVennDiagram)
 
-setwd('D:\\03_mtDNA\\bigcsMT\\analysis\\mtDNA_BIGCS_II_4x100\\output\\outputfile')
-
-dataf = read.table('seven_dataset.csv', sep = ',', header = 1)
-tail(dataf$BIGCS_II[dataf$BIGCS_II != ""])
-dataf$BIGCS_II[dataf$BIGCS_II != ""]
+dataf = read.table('../data/seven_dataset.csv', sep = ',', header = 1)
 
 colnames(dataf)
-x <- list(`BIGCS II (5218)`=dataf$BIGCS_II[dataf$BIGCS_II != ""],
-          `1KGP (2976)`=dataf$X1000G[dataf$X1000G != ""],
-          `GnomAD (10850)`=dataf$Gnomad[dataf$Gnomad != ""],
-          `Helix (14132)`=dataf$Helix[dataf$Helix != ""],
-          `HmtVar (40451)`=dataf$Hmtvar[dataf$Hmtvar != ""],
-          `MitoMap (19321)`=dataf$Mitomap[dataf$Mitomap != ""],
-          `NyuWa (7,266)`=dataf$NyuWa[dataf$NyuWa != ""])
+x <- list(`A (5218)`=dataf$A[dataf$A != ""],
+          `B (2976)`=dataf$B[dataf$B != ""],
+          `C (10850)`=dataf$C[dataf$C != ""],
+          `D (14132)`=dataf$D[dataf$D != ""],
+          `E (40451)`=dataf$E[dataf$E != ""],
+          `F (19321)`=dataf$F[dataf$F != ""],
+          `G (7,266)`=dataf$G[dataf$G != ""])
 
 library(ggplot2)
 ggVennDiagram(x, label = "count",label_alpha=0,label_geom='label',edge_size=1.5,
@@ -22,8 +20,6 @@ ggVennDiagram(x, label = "count",label_alpha=0,label_geom='label',edge_size=1.5,
                             "#d62728", "#9467bd", "#8c564b", 
                             "#e377c2"))+#scale_fill_distiller(palette = "PiYG")
   scale_fill_gradientn(colours = c("white")) + theme(legend.position = '')
-
-?ggVennDiagram
 
 
 
